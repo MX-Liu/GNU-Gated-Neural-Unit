9722bcaaf50a7f293812b236eb2871bb05da7c0e
dfki <liuxx0123@gmail.com>
2025-07-27 14:10:50 +0200

    changed data type in args

--- run_all.sh
+++ run_all.sh
@@ -1,1 +1,2 @@
-./run.sh --classifier mlp --num_epochs 20 
+# study the dataset size
+./run.sh --num_epochs 20 --ds_percentage 0.1 --grid 5 --degree 3 --noise 0.0 --classifier mlp --activation relu --alpha1 0.5 --alpha2 0.5 --seed 1
