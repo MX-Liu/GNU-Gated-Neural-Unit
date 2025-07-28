cb0071e34c716377de005d1f3414f44e4611e332
dfki <liuxx0123@gmail.com>
2025-07-28 08:30:59 +0200

    test epoch 20

--- run_single.sh
+++ run_single.sh
@@ -1,4 +1,4 @@
-srun -K  --job-name=GNU --partition=batch --gpus=1 --cpus-per-task=2 --mem=32G --time=2-00:00 --output output/console.%A_%a.out --error output/console.%A_%a.error  \
+srun -K  --job-name=GNU --partition=batch,RTX3090,V100-32GB --gpus=1 --cpus-per-task=2 --mem=32G --time=2-00:00 --output output/console.%A_%a.out --error output/console.%A_%a.error  \
           --container-image=/netscratch/enroot/nvcr.io_nvidia_pytorch_23.02-py3.sqsh \
           --container-workdir=`pwd` \
           --container-mounts=/netscratch:/netscratch,/ds:/ds,`pwd`:`pwd` \
