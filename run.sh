2b214f4827424d1831d6f1819c1725773548eeb3
Mengxi Liu <liuxx0123@gmail.com>
2026-07-15 16:46:11 +0200

    deploy on cluster

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-srun -K  --job-name=GNU --partition=RTXA6000,RTXA6000-EI --gpus=1 --cpus-per-task=12 --mem=64G --time=2-00:00 --output output/console.%A_%a.out --error output/console.%A_%a.error  \
+srun -K  --job-name=GNU --partition=A100-PCI,RTXA6000,RTXA6000-EI,B200-TRU --gpus=1 --cpus-per-task=6 --mem=64G --time=2-00:00 --output output/console.%A_%a.out --error output/console.%A_%a.error  \
           --container-image=/netscratch/enroot/nvcr.io_nvidia_pytorch_23.02-py3.sqsh \
           --container-workdir=`pwd` \
           --container-mounts=/netscratch:/netscratch,/ds:/ds,`pwd`:`pwd` \
--- run_all_ablation.sh
+++ run_all_ablation.sh
@@ -1,2 +1,23 @@
 # study the dataset size
-./run.sh --num_epochs 20 --ds_percentage 0.1 --grid 5 --degree 3 --noise 0.0 --classifier gnu --activation silu --alpha1 0.5 --alpha2 0.5 --seed 1 --fixed_alpha
+./run.sh --dataset cifar100 --num_epochs 20 --ds_percentage 0.1 --grid 5 --degree 3 --noise 0.0 --classifier gnu --activation silu --alpha1 0.5 --alpha2 0.5 --seed 1 --fixed_alpha
+# ./run.sh --dataset cifar100 --num_epochs 20 --ds_percentage 0.1 --grid 5 --degree 3 --noise 0.0 --classifier gnu --activation silu --alpha1 0.5 --alpha2 0.5 --seed 0 --fixed_alpha
+# ./run.sh --dataset cifar100 --num_epochs 20 --ds_percentage 0.1 --grid 5 --degree 3 --noise 0.0 --classifier gnu --activation silu --alpha1 0.5 --alpha2 0.5 --seed 2 --fixed_alpha
+
+# # study the dataset size
+# ./run.sh --dataset cifar100 --num_epochs 20 --ds_percentage 0.1 --grid 5 --degree 3 --noise 0.0 --classifier gnu --activation silu --alpha1 0.25 --alpha2 0.75 --seed 1 --fixed_alpha
+# ./run.sh --dataset cifar100 --num_epochs 20 --ds_percentage 0.1 --grid 5 --degree 3 --noise 0.0 --classifier gnu --activation silu --alpha1 0.25 --alpha2 0.75 --seed 0 --fixed_alpha
+# ./run.sh --dataset cifar100 --num_epochs 20 --ds_percentage 0.1 --grid 5 --degree 3 --noise 0.0 --classifier gnu --activation silu --alpha1 0.25 --alpha2 0.75 --seed 2 --fixed_alpha
+
+# # study the dataset size
+# ./run.sh --dataset cifar100 --num_epochs 20 --ds_percentage 0.1 --grid 5 --degree 3 --noise 0.0 --classifier gnu --activation silu --alpha1 0.75 --alpha2 0.25 --seed 1 --fixed_alpha
+# ./run.sh --dataset cifar100 --num_epochs 20 --ds_percentage 0.1 --grid 5 --degree 3 --noise 0.0 --classifier gnu --activation silu --alpha1 0.75 --alpha2 0.25 --seed 0 --fixed_alpha
+# ./run.sh --dataset cifar100 --num_epochs 20 --ds_percentage 0.1 --grid 5 --degree 3 --noise 0.0 --classifier gnu --activation silu --alpha1 0.75 --alpha2 0.25 --seed 2 --fixed_alpha
+
+# # study the dataset size
+# ./run.sh --dataset cifar100 --num_epochs 20 --ds_percentage 0.1 --grid 5 --degree 3 --noise 0.0 --classifier gnu --activation silu --alpha1 0.0 --alpha2 1.0 --seed 1 --fixed_alpha
+# ./run.sh --dataset cifar100 --num_epochs 20 --ds_percentage 0.1 --grid 5 --degree 3 --noise 0.0 --classifier gnu --activation silu --alpha1 0.0 --alpha2 1.0 --seed 0 --fixed_alpha
+# ./run.sh --dataset cifar100 --num_epochs 20 --ds_percentage 0.1 --grid 5 --degree 3 --noise 0.0 --classifier gnu --activation silu --alpha1 0.0 --alpha2 1.0 --seed 2 --fixed_alpha
+
+# ./run.sh --dataset cifar100 --num_epochs 20 --ds_percentage 0.1 --grid 5 --degree 3 --noise 0.0 --classifier gnu --activation silu --alpha1 1.0 --alpha2 0.0 --seed 1 --fixed_alpha
+# ./run.sh --dataset cifar100 --num_epochs 20 --ds_percentage 0.1 --grid 5 --degree 3 --noise 0.0 --classifier gnu --activation silu --alpha1 1.0 --alpha2 0.0 --seed 0 --fixed_alpha
+# ./run.sh --dataset cifar100 --num_epochs 20 --ds_percentage 0.1 --grid 5 --degree 3 --noise 0.0 --classifier gnu --activation silu --alpha1 1.0 --alpha2 0.0 --seed 2 --fixed_alpha
