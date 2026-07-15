12d968e6842222431af1b5634fd3bb815d13a6a0
Mengxi Liu <liuxx0123@gmail.com>
2026-07-15 21:40:32 +0200

    conf new gpus

--- run_single.sh
+++ run_single.sh
@@ -1,4 +1,4 @@
-srun -K  --job-name=GNU --partition=batch,RTXA6000,RTXA6000-EI,A100-RP,A100-PCI,L40S,B200-TRU --gpus=1 --cpus-per-task=2 --mem=32G --time=2-00:00 --output output/console.%A_%a.out --error output/console.%A_%a.error  \
+srun -K  --job-name=GNU --partition=RTXB6000,V100-32GB,batch,RTXA6000,RTXA6000-EI,A100-RP,A100-PCI,L40S,B200-TRU --gpus=1 --cpus-per-task=2 --mem=32G --time=2-00:00 --output output/console.%A_%a.out --error output/console.%A_%a.error  \
           --container-image=/netscratch/enroot/nvcr.io_nvidia_pytorch_23.02-py3.sqsh \
           --container-workdir=`pwd` \
           --container-mounts=/netscratch:/netscratch,/ds:/ds,`pwd`:`pwd` \
